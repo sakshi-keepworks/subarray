349925020874d0644519e76db265d93a5de7f3d1
sjain1107 <sakshijain1107@gmail.com>
2017-09-02 16:58:11 +0530

    Add execution file

---
+++ lib/execute.rb
@@ -0,0 +1,10 @@
+require './lib/max_contiguous_subarray'
+
+csa = ContiguousSubarray.new
+input = " "
+while !input.empty?
+  input = gets.chomp.split().map { |e| e.to_i }
+  csa.raw_array.push input
+end
+result = csa.largest_contiguous_subarray_sum
+puts result
